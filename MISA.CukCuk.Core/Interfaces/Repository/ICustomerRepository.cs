56a6f27f67991ed880453fe38083f82fb1fe2aff
DuongCoder <gduongit9@gmail.com>
2021-05-20 00:29:04 +0700

    dependence

---
+++ MISA.CukCuk.Core/Interfaces/Repository/ICustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.CukCuk.Core.Interfaces.Repository
+{
+    interface ICustomerRepository
+    {
+        /// <summary>
+        /// Khởi tạo kết nối tới db
+        /// </summary>
+        /// CreatedBy: DuongNG (20/05/2021)
+        void InitConnection();
+
+        bool CheckCustomerCodeExist(string customerCode);
+        bool CheckEmailExists(String email);
+    }
+}
